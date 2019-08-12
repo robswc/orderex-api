398395d15e399893d4d87469d9c61b23ea2df32e
Robswc <robert.s.w.carroll@gmail.com>
2019-08-11 23:02:06 -0400

    removed misc files

--- orderex-api/engine/interface/config.py
+++
@@ -1,9 +0,0 @@
-keys = {'bitmex': 'api_key',
-        'kraken': ''}
-
-proxy = {'un': '', 'pw': '', 'host': '', 'port': ''}
-
-ip_whitelist = {}
-
-# It is heavily discouraged to not use a pin.
-users = {}
